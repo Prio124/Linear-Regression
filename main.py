89aa4330f26f04a7044aefc9be66b63d04746f73
Prio124 <priolosalvatore1@libero.it>
2020-09-16 09:52:15 +0200

    Class creation
    I've started coding several methods of the class.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
     File name: main.py
     Author: Giuseppe Priolo
     Date created: 15/09/2020
-    Date last modified: 15/09/2020
+    Date last modified: 16/09/2020
     Python Version: 3.8.5
 '''
 #   This program predicts a function which is based on a given data set.
@@ -12,52 +12,54 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 
-def create_a_function() :
-    #   Create a random functon
-    #   Random matrix
-    f = np.random.rand(50, 2)
-    #Extract the 'x' and 'y' columns
-    x = f[:, 0] * 100
-    y = f[:, 1] * 100
+#Create a class
+class linearRegression:
 
-    return(x,y)
+    def insert_data(self, x, y):
+        self.x = x
+        self.y = y
 
-def function_plot(m, q, px, py) :
-    x = np.array(range(100))
-    for n in x :
-        y = m*x + q
-    plt.plot(x, y)
-    plt.plot(px, py, 'ro')
-    plt.show()
+    def create_a_function(self):
+        #   Create a random functon
+        #   Random matrix
+        f = np.random.rand(100, 2)
+        # Extract the 'x' and 'y' columns
+        self.x = f[:, 0] * 1000
+        self.y = f[:, 1] * 1000
 
-def f_prediction(x, y) :
-    #All the formulas are in the description
-    #f(x) = mx + q
+        return (self.x, self.y)
 
-    #Sample Values
-    #x = np.array([1,2,3])
-    #y=x
+    def f_prediction(self):
+        # All the formulas are in the description
+        # f(x) = mx + q
+        x = self.x
+        y = self.y
+        # Sample Values
+        # x = np.array([1,2,3])
+        # y=x
 
-    av_x = np.sum(x)/len(x)
-    av_y = np.sum(y)/len(y)
-    pow_x = x*x
-    av_pow_x = np.sum(pow_x)/len(x)
-    xy = x * y
-    av_xy = np.sum(xy)/len(xy)
+        av_x = np.sum(self.x) / len(self.x)
+        av_y = np.sum(self.y) / len(self.y)
+        pow_x = self.x * self.x
+        av_pow_x = np.sum(pow_x) / len(x)
+        xy = self.x * self.y
+        av_xy = np.sum(xy) / len(xy)
+        #Print results of the sample
+        #print(x,y,xy,av_x,av_y,av_pow_x,av_xy)
+        #Calculate m
+        self.m = (av_xy - av_x * av_y) / (av_pow_x - av_x ** 2)
+        # Calculate q
+        self.q = self.y[0] - self.m * self.x[0]
 
-    #print(x,y,xy,av_x,av_y,av_pow_x,av_xy)
+        return (self.m, self.q)
 
-    m = (av_xy-av_x*av_y)/(av_pow_x-av_x**2)
-    q = y[0] - m*x[0]
+    def function_plot(self):
+        px = np.array(range(1000))
+        for n in px:
+            py = self.m * px + self.q
+        #Show [x, y] points
+        plt.plot(self.x, self.y, 'ro')
+        #Show the predicted function
+        plt.plot(px, py)
+        plt.show()
 
-    return (m,q)
-
-
-#Test Variables
-#f = create_a_function()
-#q = f_prediction(f[0], f[1])
-#function_plot(q[0], q[1], f[0], f[1])
-#function_plot(f[0], f[1])
-#csv = {'x': [1,2,3,4],
-        'y': [1,2,3,4]
-#df = pandas.DataFrame(csv, columns=['x','y'])
